e4aee3946adecedc97a6ad89092213a71ea2c9ab
Samir Maoude <samir.maoude@manobi.com>
2023-12-04 21:23:27 +0100

    day4

---
+++ aoc_2023/day4/p2.py
@@ -0,0 +1,68 @@
+# Advent of Code template by @MathisHammel
+
+import requests
+
+from ..secrets.aoc_secrets import AOC_COOKIE # Put your session cookie in this variable
+YEAR = '2023'
+
+def get_input(day):
+    req = requests.get(f'https://adventofcode.com/{YEAR}/day/{day}/input', headers={'cookie':'session='+AOC_COOKIE})
+    return req.text
+
+def get_example(day,offset=0):
+    req = requests.get(f'https://adventofcode.com/{YEAR}/day/{day}', headers={'cookie':'session='+AOC_COOKIE})
+    return req.text.split('<pre><code>')[offset+1].split('</code></pre>')[0]
+
+def submit(day, level, answer):
+    input(f'You are about to submit the follwing answer:\n>>>>>>>>>>>>>>>>> {answer}\nPress enter to continue or Ctrl+C to abort.')
+    data = {
+      'level': str(level),
+      'answer': str(answer)
+    }
+
+    response = requests.post(f'https://adventofcode.com/{YEAR}/day/{day}/answer', headers={'cookie':'session='+AOC_COOKIE}, data=data)
+    if 'You gave an answer too recently' in response.text:
+        print('VERDICT : TOO MANY REQUESTS')
+    elif 'not the right answer' in response.text:
+        if 'too low' in response.text:
+            print('VERDICT : WRONG (TOO LOW)')
+        elif 'too high' in response.text:
+            print('VERDICT : WRONG (TOO HIGH)')
+        else:
+            print('VERDICT : WRONG (UNKNOWN)')
+    elif 'seem to be solving the right level.' in response.text:
+        print('VERDICT : INVALID LEVEL')
+    else:
+        print('VERDICT : OK !')
+
+def ints(s):
+    return list(map(int, s.split()))
+
+DAY = 4
+PART = 2
+s = get_input(DAY).strip()
+
+import collections
+import math
+from collections import defaultdict
+#import networkx as nx
+
+# Your code here
+total = [1] * len(s.splitlines())
+for idx, line in enumerate(s.splitlines()):
+    _, card = line.split(': ')
+    win, have = card.split(' | ')
+
+    win = set(map(int, win.split()))
+    have = set(map(int, have.split()))
+
+    inter  = win.intersection(have)
+
+    n = len(inter)
+    
+    if n>0:
+        for j in range(idx+1, min(idx+n+1, len(s.splitlines()))):
+            total[j] += total[idx]
+
+ans = sum(total)
+submit(DAY, PART, ans)
